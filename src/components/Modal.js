078076baaf4a4b38e40b41726f989f0f86813c39
kitajchuk <bk@kitajchuk.com>
2021-03-18 21:55:41 -0700

    more responsive modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,11 +6,11 @@ export default function Modal({abortHandler, confirmHandler}) {
     <div className="punxy__modal">
       <div className="punxy__modal__wrap">
         <Masthead>
-          <h1><strong>know what you're doing</strong>. seeding disperses your current transactions out to all your peers. likewise, your local store will be replenished with your peers' seeds. fight the power.</h1>
+          <h1>seeding disperses your current transactions out to all your peers. likewise, your local store will be replenished with your peers' seeds. fight the power.</h1>
         </Masthead>
         <div className="punxy__btns">
           <Button handler={abortHandler}>nevermind...</Button>
-          <Button handler={confirmHandler}>lets do it!</Button>
+          <Button handler={confirmHandler}>need to seed</Button>
         </div>
       </div>
     </div>
