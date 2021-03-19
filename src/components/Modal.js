5bddf37d5eee0640f80380865f7fbd811140539c
kitajchuk <bk@kitajchuk.com>
2021-03-18 18:40:54 -0700

    nevermind...

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,7 @@ export default function Modal({abortHandler, confirmHandler}) {
           <h1><strong>know what you're doing</strong>. seeding disperses your current transactions out to all your peers. likewise, your local store will be replenished with your peers' seeds. fight the power.</h1>
         </Masthead>
         <div className="punxy__btns">
-          <Button handler={abortHandler}>nevermind</Button>
+          <Button handler={abortHandler}>nevermind...</Button>
           <Button handler={confirmHandler}>lets do it!</Button>
         </div>
       </div>
