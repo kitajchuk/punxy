ebc9e467763a2fe50527eb85524e77928fc8aded
Brandon Lee Kitajchuk <bk@kitajchuk.com>
2022-04-29 03:18:07 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,11 +44,7 @@ export function Jobs() {
       <Masthead>
         <h1>fancy yourself a freelancer with illusions standing? look no further.</h1>
       </Masthead>
-      <Feed 
-        endpoint="jobstories"
-        loading1="locating signals in the noise..."
-        loading2="indexing the dark net bounty board..."
-      />
+      <Feed endpoint="jobstories" />
     </>
   );
 }
